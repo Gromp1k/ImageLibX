ac745281f8a26e38dd4f4b5ceb09bf134b4ef3e5
Gromp1k <kacperducin@gmail.com>
2024-10-02 20:42:26 +0200

    Initial Commit

---
+++ ImageLibX/ImageLibXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageLibXApp.swift
+//  ImageLibX
+//
+//  Created by Kacper on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageLibXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
